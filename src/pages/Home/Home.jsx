6851458711b63e3e310b941826f4226414cbc32f
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-08-08 14:20:18 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import './Home.scss';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const btnText = 'Go to countries table';
+  const btnText = 'Start now';
 
   return (
     <>
