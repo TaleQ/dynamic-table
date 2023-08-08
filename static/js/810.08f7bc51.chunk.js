b8f4954856df703008c6028fbf60c1d4b6d6b5b9
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-08-08 14:23:12 +0200

    Updates

--- static/js/810.08f7bc51.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdynamic_table=self.webpackChunkdynamic_table||[]).push([[810],{3810:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var s=t(1087),a=t(184),c=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsxs)("div",{className:"home-thumb",children:[(0,a.jsx)("h1",{className:"home-title",children:"Countries Search"}),(0,a.jsx)(s.rU,{className:"home-link",to:"/countries",children:"Go to countries table".toUpperCase()})]})})}}}]);
-//# sourceMappingURL=810.08f7bc51.chunk.js.map
